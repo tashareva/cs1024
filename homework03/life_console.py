03b54180344dc1785c442e2dee922463bfe3afde
Angelina-Gordienko <aigordienko@mail.ru>
2021-01-23 23:11:35 +0300

    console

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -10,13 +10,51 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        screen.clear()
+        y, x = screen.getmaxyx()
+        doc = ""
+
+        for row in range(y):
+            for col in range(x):
+                if row == 0 or row == (y - 1):
+                    if col == 0 or col == x:
+                        doc += "*"
+                    else:
+                        doc += "-"
+                elif row < (y - 1) and row > 0:
+                    if col == 0 or col == (x - 1):
+                        doc += "|"
+                    else:
+                        doc += " "
+            try:
+                screen.addstr(doc)
+            except curses.error:
+                pass
+            doc = ""
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        y, x = screen.getmaxyx()
+        col = (x - self.life.cols) // 2
+        row = (y - self.life.rows) // 2
+        for count_row, just_row in enumerate(self.life.curr_generation):
+            for count_col, just_col in enumerate(just_row):
+                if just_col:
+                    try:
+                        screen.addstr(count_row + row, count_col + col, "*")
+                    except curses.error:
+                        pass
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        curses.wrapper(self.draw_borders)
+        while not self.life.is_max_generations_exceeded and self.life.is_changing:
+            self.life.step()
+            self.draw_borders(screen)
         curses.endwin()
+
+
+if __name__ == "__main__":
+    life = GameOfLife((24, 80), max_generations=50)
+    gui = Console(life)
+    gui.run()
