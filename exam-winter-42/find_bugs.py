3616764d9c2b04020f352cdfe3650ab701eaffeb
Angelina-Gordienko <aigordienko@mail.ru>
2021-01-30 12:28:25 +0300

    bugs

---
+++ exam-winter-42/find_bugs.py
@@ -0,0 +1,13 @@
+def find_bugs(task: list, correct_task: list) -> list:
+    task = []
+    correct_task = [A -> B, B->C, C->+]
+    for task: 
+        if task == correct_task:
+            print('No problems')
+        elif task = [A->B, B->C, C->B]:
+            task = [A -> B, B->C, C->+]
+        elif task = [A->A, B->C, C->A]:
+            print('More than 1 mistake')
+        elif task = [A->A, B->B]:
+            print('More than 1 mistake')
+    return task 
