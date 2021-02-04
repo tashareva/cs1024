0872e0ab6d42c0fdd2998ed578c0c2bf05ff7d64
Angelina-Gordienko <aigordienko@mail.ru>
2021-02-04 20:19:59 +0300

    exceptions

--- homework05/vkapi/session.py
+++ homework05/vkapi/session.py
@@ -4,6 +4,23 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
+from vkapi.config import VK_CONFIG
+
+
+class TimeoutHTTPAdapter(HTTPAdapter):
+    def __init__(self, timeout, *args, **kwargs):
+        self.timeout = timeout
+        if "timeout" in kwargs:
+            self.timeout = kwargs["timeout"]
+            del kwargs["timeout"]
+        super().__init__(*args, **kwargs)
+
+    def send(self, request, **kwargs):
+        timeout = kwargs.get("timeout")
+        if timeout is None:
+            kwargs["timeout"] = self.timeout
+        return super().send(request, **kwargs)
+
 
 class Session:
     """
@@ -22,10 +39,22 @@ class Session:
         max_retries: int = 3,
         backoff_factor: float = 0.3,
     ) -> None:
-        pass
+        super().__init__()
+        self.base_url = base_url
+        self.timeout = timeout
 
-    def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
-        pass
+        retry = Retry(
+            total=max_retries,
+            status_forcelist=[429, 500, 502, 503, 504],
+            backoff_factor=backoff_factor,
+            method_whitelist=["HEAD", "GET", "OPTIONS", "POST"],
+        )
 
-    def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
-        pass
+        adapter = TimeoutHTTPAdapter(timeout=timeout, max_retries=retry)
+        self.mount = (self.base_url, adapter)
+
+    def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:  # type: ignore
+        return super().get(self.base_url + "/" + url, *args, **kwargs)
+
+    def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:  # type: ignore
+        return super().post(self.base_url + "/" + url, *args, **kwargs)
