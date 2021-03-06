02172f62addd542aef8b03e08c9c2cfa14fd090f
Angelina-Gordienko <aigordienko@mail.ru>
2021-03-06 15:41:17 +0300

    config

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -1,7 +1,3 @@
 # https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
 
-VK_CONFIG = {
-    "domain": "https://api.vk.com/method",
-    "access_token": "a1989fd89f485e5c72c850eee2e669a0a53c1426dfec994484f7636b8cb0ecefc70f8bd6c76b673f9b5d1",
-    "version": "5.126",
-}
+VK_CONFIG = {"domain": "https://api.vk.com/method", "access_token": "", "version": "5.126"}
